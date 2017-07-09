6e6fdf9e73803b3330c203a2f63a636660f449e3
13083513833@163.com <15601597255ab*c>
2017-07-09 10:32:13 +0800

    Initial commit

---
+++ app/src/main/java/com/alpha/live/Constants.java
@@ -0,0 +1,25 @@
+package com.alpha.live;
+
+/**
+ * Created by lsj on 17-6-18.
+ */
+public class Constants {
+    //appid 微信分配的公众账号ID
+    //public static final String APP_ID = "wx73b78c39451e954d";
+    public static final String APP_ID = "wx14af47ab474a0991";
+
+    //商户号 微信分配的公众账号ID
+    public static final String MCH_ID = "1415675302";
+
+    //  API密钥，在商户平台设置
+    public static final  String API_KEY= "ganzhoualpha112114115WXZHOUALPHA";
+    /*
+    public static final String APP_ID = "public static final String APP_ID = \"wx73b78c39451e954d\";";
+
+    //商户号 微信分配的公众账号ID
+    public static final String MCH_ID = "1448188402";
+
+    //  API密钥，在商户平台设置
+    public static final  String API_KEY= "njsbz2abcdefghijklmnopqrstuvwxyz";
+    */
+}
